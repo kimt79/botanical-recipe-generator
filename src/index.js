346814d81cdba59de93ff0132215feed8e195f7a
kimt79 <164622332+kimt79@users.noreply.github.com>
2024-08-30 11:06:53 -0500

    enhanced user experience

--- src/index.js
+++ src/index.js
@@ -17,11 +17,12 @@ function generateRecipe(event) {
     "You are an expert in all things botanical. You are a master chef who loves to use roots, herbs and spices in cooking. Your mission is to generate a recipe in basic HTML format without saying HTML. Make sure to follow user instructions.";
 
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  let recipeElement = document.querySelector("#recipe");
+  recipeElement.classList.remove("hidden");
+  recipeElement.innerHTML = `<div class="generating">Cooking is magic! Generating a recipe for ${instructionsInput.value}</div>`;
 
   axios.get(apiUrl).then(displayRecipe);
 }
-console.log = "Generating Recipe";
-console.log = `prompt:${prompt}`;
 
 let recipeFormElement = document.querySelector("#recipe-generator-form");
 recipeFormElement.addEventListener("submit", generateRecipe);
